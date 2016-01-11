692b92b674ca254f3ca172f29d070c04543e73c4
Raja Appuswamy <raja.appuswamy@epfl.ch>
2016-01-11 21:38:31 +0100

    bug fix with tlock implementation

--- tlock.c
+++ tlock.c
@@ -7,16 +7,16 @@ void tlock_init(tlock_t *lock)
   lock->ticket = lock->users = 0;
 }
 
-void ticket_lock(tlock_t *t)
+void tlock_acquire(tlock_t *t)
 {
   unsigned short me = __sync_fetch_and_add(&t->users, 1);
   
   while (t->ticket != me) _mm_pause();
 }
 
-void ticket_unlock(tlock_t *t)
+void tlock_release(tlock_t *t)
 {
-  t->ticket++;
+  __sync_fetch_and_add(&t->ticket, 1);
 }
 
 #endif
