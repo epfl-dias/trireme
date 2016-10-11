8cd3c5dcc2e3d306f8b1a9b8560765a2e43bf288
Raja Appuswamy <raja.appuswamy@epfl.ch>
2016-10-11 16:39:10 +0200

    Add missing file

---
+++ glo.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#if defined(__MAIN__)
+#define EXTERN
+#else
+#define EXTERN extern
+#endif
+
+EXTERN int g_nservers;
+EXTERN int g_nrecs;
+EXTERN struct benchmark *g_benchmark;
+EXTERN struct hash_table *hash_table;
+EXTERN double g_alpha;
+EXTERN int g_niters;
+EXTERN int g_nhot_servers;
+EXTERN int g_nhot_recs;
+EXTERN int g_ops_per_txn;
+EXTERN int g_nremote_ops;
+EXTERN int g_dist_threshold;
+EXTERN int g_write_threshold;
+EXTERN int g_batch_size;
+
+
