781d9028003b0a8d3b19146763ab0136fa5afa76
Lionel Sambuc <lionel.sambuc@epfl.ch>
2018-08-13 11:58:34 +0200

    First go at documenting the ring buffer API

--- src/lib/libmsg/ring_buffer.h
+++ src/lib/libmsg/ring_buffer.h
@@ -12,16 +12,39 @@ struct box {
   struct ring_buffer * out;
 } __attribute__ ((aligned (CACHELINE)));
 
+/* Allocate a ring buffer of size RING_BUFFER_SIZE.
+ * buffer: OUT parameter. */
 void ring_buffer_alloc(struct ring_buffer** buffer);
+
+/* Free the ring buffer pointed to by buffer.
+ * buffer: OUT parameter, on success it is set to NULL. */
 void ring_buffer_free(struct ring_buffer** buffer);
 
+/* Write the message data into the buffer. */
 void ring_buffer_write(struct ring_buffer* buffer, uint64_t data);
-void ring_buffer_write_all(struct ring_buffer* buffer, int32_t write_count, const uint64_t* data, int32_t force_flush);
+
+/* Write 'count' messages stored in the 'data' array into the buffer. If
+ * 'force_flush' is non-zero, a flush will be executed immediately. */
+void ring_buffer_write_all(struct ring_buffer* buffer, int32_t write_count,
+	const uint64_t* data, int32_t force_flush);
+
+/* Flush the ring buffer, making sure the data is visible to the recipient. */
 void ring_buffer_flush(struct ring_buffer* buffer);
 
-int32_t ring_buffer_read_all(struct ring_buffer* buffer, int32_t max_read_count, uint64_t* data, int32_t blocking);
+/* Read up to 'max_read_count' messages in the 'data' array. If 'blocking' is
+ * set to 0, then the function will always return immediately, only providing
+ * the data currently available, instead of waiting to fill max_read_count
+ * values.*/
+int32_t ring_buffer_read_all(struct ring_buffer* buffer,
+	int32_t max_read_count, uint64_t* data, int32_t blocking);
 
-int32_t ring_buffer_peek(struct ring_buffer* buffer, int32_t max_read_count, uint64_t* data);
+/* Read up to 'max_read_count' messages in the 'data' array, but unlike 
+ * ring_buffer_read_all(), it will never block, nor will it advance the
+ * number of messages read from the buffer. */
+int32_t ring_buffer_peek(struct ring_buffer* buffer, int32_t max_read_count,
+	uint64_t* data);
+
+/* Seek the current read position to the current position plus 'count'. */
 void ring_buffer_seek(struct ring_buffer *buffer, int32_t count);
 
 #endif
